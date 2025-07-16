7e5d0ae08cae6daa87687a121dbbadcc809e3c94
Albert Mitton <albertmitton78@gmail.com>
2025-07-16 12:07:51 +1200

    Refactor player node to remove hardcoded values for dimensions and sprite sizes

--- player/player.gd
+++ player/player.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
+# Variables that need to be instantiated in child nodes
 var starting_position: Vector2
 var down_key: String
 var up_key: String
+
+# Constants
+const paddle_speed = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,11 +15,16 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	var arena_lower_boundary = get_viewport_rect().end.y
+	var arena_upper_boundary = get_viewport_rect().position.y
+	var paddle_sprite: Sprite2D = get_node("PongPaddle")
+	var paddle_texture: Texture = paddle_sprite.texture
+	var paddle_size = paddle_texture.get_height() * paddle_sprite.scale.y
 	
 	# Move down on player pressing s button
-	if (Input.is_action_pressed(down_key) and position.y + 64 < 480):
-		position.y += 100 * delta
+	if (Input.is_action_pressed(down_key) and position.y + paddle_size < arena_lower_boundary):
+		position.y += paddle_speed * delta
 	
 	# Move up on player pressing w button
-	if (Input.is_action_pressed(up_key) and position.y > 0):
-		position.y -= 100 * delta
+	if (Input.is_action_pressed(up_key) and position.y > arena_upper_boundary):
+		position.y -= paddle_speed * delta
