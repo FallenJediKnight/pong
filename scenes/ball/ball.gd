82cbfd273cb6395be95631b4277883ae6f9a4217
Albert Mitton <albertmitton78@gmail.com>
2025-07-19 12:56:30 +1200

    Make ball random position and direction more sensible

--- scenes/ball/ball.gd
+++ scenes/ball/ball.gd
@@ -4,6 +4,8 @@ const BALL_SPEED = 400
 
 var ball_direction
 var mid_point_x
+var viewport_start_y
+var viewport_end_y
 
 signal goal_scored_by_player(n: int)
 
@@ -13,6 +15,8 @@ func _ready() -> void:
 	ball_direction = get_random_starting_direction()
 	linear_velocity = ball_direction * BALL_SPEED
 	mid_point_x = get_viewport_rect().end.x * 0.5
+	viewport_start_y = get_viewport_rect().position.y
+	viewport_end_y = get_viewport_rect().end.y
 	global_position = Vector2(mid_point_x, randf())
 
 func _physics_process(delta: float) -> void:
@@ -26,9 +30,9 @@ func _physics_process(delta: float) -> void:
 		reset_ball_position_and_direction()
 
 func get_random_starting_direction() -> Vector2:
-	return Vector2(randf(), randf())
+	return Vector2(randf_range(0.5, 1), randf_range(0.5, 1))
 
 func reset_ball_position_and_direction() -> void:
-	global_position = Vector2(mid_point_x, randf())
+	global_position = Vector2(mid_point_x, randi_range(viewport_start_y, viewport_end_y))
 	ball_direction = get_random_starting_direction()
 	linear_velocity = ball_direction * BALL_SPEED
